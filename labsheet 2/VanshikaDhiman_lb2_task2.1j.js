5091e8ce0cf6b60e2e4420cb75d38813601ab41d
VanshikaDhiman14 <cu24250228@coeruniversity.ac.in>
2026-08-20 11:36:08 +0530

    Add files via upload
    Labsheet2 Task2.1 is completed

---
+++ labsheet 2/VanshikaDhiman_lb2_task2.1j.js
@@ -0,0 +1,9 @@
+const themeToggle=document.getElementById("themeToggle");
+themeToggle.addEventListener("click",function(){
+    document.body.classList.toggle("light-mode");
+    if(document.body.classList.contains("light-mode")){
+        themeToggle.textContent="Dark Mode";
+    }else{
+        themeToggle.textContent="Light Mode";
+    }
+});
